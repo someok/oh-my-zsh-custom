c29d2da42ebff3ec676a5fd39ff00d86a7610cb2
JX Wang <wjianxu@gmail.com>
2018-12-19 02:22:23 +0800

    feat: 启用 oh-my-zsh 中一些常用 plugin

--- oh-my-zsh-custom.sh
+++ oh-my-zsh-custom.sh
@@ -5,20 +5,6 @@ SOMEOK_ZSH=${0:A:h}
 
 export ZSH=${SOMEOK_ZSH}/oh-my-zsh
 
-# linux or mac os x
-if [ "$(uname -s)" = "Darwin" ]; then
-	# theme: https://github.com/eendroroy/alien
-    export ALIEN_THEME="gruvbox"
-    export ALIEN_DATE_TIME_FORMAT=%H:%M:%S # default is %r
-    export USE_NERD_FONT=1
-    source ${SOMEOK_ZSH}/modules/alien/alien.zsh
-else
-    ZSH_THEME="someok-linux"
-
-    # 关闭 linux 下的安全监测
-    ZSH_DISABLE_COMPFIX=true
-fi
-
 # Uncomment the following line if you want to change the command execution time
 # stamp shown in the history command output.
 # The optional three formats: "mm/dd/yyyy"|"dd.mm.yyyy"|"yyyy-mm-dd"
@@ -29,13 +15,48 @@ ZSH_CUSTOM=${SOMEOK_ZSH}/custom
 
 plugins=(
     autojump
-    gnu-utils
+    extract
     sudo
+    history
+    copydir
+    copybuffer
+    colored-man-pages
     zsh-syntax-highlighting
     zsh-autosuggestions
 )
 
+# linux or mac os x
+if [ "$(uname -s)" = "Darwin" ]; then
+    # mac os x
+	# theme: https://github.com/eendroroy/alien
+    export ALIEN_THEME="gruvbox"
+    export ALIEN_DATE_TIME_FORMAT=%H:%M:%S # default is %r
+    export USE_NERD_FONT=1
+    source ${SOMEOK_ZSH}/modules/alien/alien.zsh
+
+    # mac 上专用 plugin
+    plugins=(
+        $plugins
+        brew
+        forklift
+        fzf
+        npm
+        sublime
+    )
+else
+    # linux
+    ZSH_THEME="someok-linux"
+
+    # 关闭 linux 下的安全监测
+    ZSH_DISABLE_COMPFIX=true
+
+fi
+
 source $ZSH/oh-my-zsh.sh
 
+# zsh-autosuggestions
 # [ctrl + space]
 bindkey '^ ' autosuggest-accept
+
+# 引入常用 aliases
+source ${SOMEOK_ZSH}/aliases.sh
