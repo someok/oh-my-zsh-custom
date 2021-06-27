0c6204f8e7f20231c00b3cfd20d51d74b2fc3430
JX Wang <wjianxu@gmail.com>
2021-06-28 01:25:03 +0800

    feat: alias 增加一些快捷命令

--- aliases.zsh
+++ aliases.zsh
@@ -7,11 +7,15 @@ alias lla='ll -A'
 alias l='ll -a'
 
 alias ls='ls --color'
+alias lS='ls -1FSsh'
+alias ldot='ls -ld .*'
 
 # 覆盖 oh-my-zsh/lib/directories.zsh 中配置
 alias la='lla'
 alias lsa='l'
 # ls end
+
+alias zshrc='${=EDITOR:-vim} ${ZDOTDIR:-$HOME}/.zshrc' # Quick access to the .zshrc file
 
 alias grep="grep --color=auto"
 
@@ -19,7 +23,9 @@ alias -s gz='tar -xzvf'
 alias -s tgz='tar -xzvf'
 alias -s bz2='tar -xjvf'
 
-alias fd='find . -type d -name'
+alias dud='du -d 1 -h'
+alias duf='du -sh *'
+(($ + commands[fd])) || alias fd='find . -type d -name'
 alias ff='find . -type f -name'
 
 alias t='tail -f'
@@ -37,3 +43,41 @@ alias -g NUL="> /dev/null 2>&1"
 
 alias t2='txt-to'
 alias qs='open -a QSpace'
+
+# Make zsh know about hosts already accessed by SSH
+zstyle -e ':completion:*:(ssh|scp|sftp|rsh|rsync):hosts' hosts 'reply=(${=${${(f)"$(cat {/etc/ssh_,~/.ssh/known_}hosts(|2)(N) /dev/null)"}%%[# ]*}//,/ })'
+
+if [ -n "$OS_MAC" ]; then
+    function free() {
+        FREE_BLOCKS=$(vm_stat | grep free | awk '{ print $3 }' | sed 's/\.//')
+        INACTIVE_BLOCKS=$(vm_stat | grep inactive | awk '{ print $3 }' | sed 's/\.//')
+        SPECULATIVE_BLOCKS=$(vm_stat | grep speculative | awk '{ print $3 }' | sed 's/\.//')
+        FREE=$((($FREE_BLOCKS + SPECULATIVE_BLOCKS) * 4096 / 1048576))
+        INACTIVE=$(($INACTIVE_BLOCKS * 4096 / 1048576))
+        TOTAL=$((($FREE + $INACTIVE)))
+        echo "Free:       $FREE MB"
+        echo "Inactive:   $INACTIVE MB"
+        echo "Total free: $TOTAL MB"
+    }
+    alias free="free"
+fi
+
+# 返回给定文件或目录的绝对路径
+function abspath() {
+    if [ -d "$1" ]; then
+        echo "$(
+            cd $1
+            pwd
+        )"
+    elif [ -f "$1" ]; then
+        if [[ $1 == */* ]]; then
+            echo "$(
+                cd ${1%/*}
+                pwd
+            )/${1##*/}"
+        else
+            echo "$(pwd)/$1"
+        fi
+    fi
+}
+alias abspath="abspath "
