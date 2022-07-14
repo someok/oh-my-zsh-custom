ae2b7e8302fd1f045eee0add16ea95c8d7a6b082
JX Wang <wjianxu@gmail.com>
2022-07-14 23:24:16 +0800

    feat: cpolar alias 迁移到 dotfiles

--- aliases.zsh
+++ aliases.zsh
@@ -45,7 +45,7 @@ alias -g CA="2>&1 | cat -A"
 alias -g NE="2> /dev/null"
 alias -g NUL="> /dev/null 2>&1"
 
-alias cpolar='~/.cpolar/cpolar'
+# alias cpolar='~/.cpolar/cpolar'
 
 if [ -n "$OS_MAC" ]; then
     function free() {
