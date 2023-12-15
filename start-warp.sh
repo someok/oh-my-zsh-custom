352ccf08ee2edd668cfe7215062c002d5dcf9ff1
JX Wang <wjianxu@gmail.com>
2023-12-15 15:18:25 +0800

    feat: 优化 warp 配置

--- start-warp.sh
+++ start-warp.sh
@@ -1,7 +1,12 @@
 OMZ_PLUGIN=${SOMEOK_ZSH}/oh-my-zsh/plugins
 
 source ${SOMEOK_ZSH}/aliases.zsh
+source ${SOMEOK_ZSH}/custom/plugins/ls-lsd/ls-lsd.plugin.zsh
+source ${SOMEOK_ZSH}/custom/plugins/cheatsh/cheatsh.plugin.zsh
+source ${SOMEOK_ZSH}/custom/plugins/git-open/git-open.plugin.zsh
+source ${SOMEOK_ZSH}/custom/plugins/colors/colors.plugin.zsh
+source ${SOMEOK_ZSH}/custom/plugins/dircolors-material/dircolors-material.plugin.zsh
 
-source ${OMZ_PLUGIN}/z/z.sh
+source ${OMZ_PLUGIN}/z/z.plugin.zsh
 source ${OMZ_PLUGIN}/extract/extract.plugin.zsh
 source ${OMZ_PLUGIN}/rsync/rsync.plugin.zsh
